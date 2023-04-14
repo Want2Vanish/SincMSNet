44d086861fecafe1e7a2927bac40adb438f3bb2a
杨明朝 <38582127+Want2Vanish@users.noreply.github.com>
2023-04-14 19:36:57 +0800

    Add files via upload

--- codes/centralRepo/networks.py
+++ codes/centralRepo/networks.py
@@ -4,11 +4,13 @@
 All network architectures: FBCNet, EEGNet, DeepConvNet
 @author: Ravikiran Mane
 """
+import math
 
 import torch
 import torch.nn as nn
+from torch.autograd import Variable
 from torchsummary import summary
-
+import numpy as np
 import sys
 
 current_module = sys.modules[__name__]
@@ -231,8 +233,71 @@ class MixedConv2d(nn.ModuleDict):
         x = torch.cat(x_out, 1)
         return x
 
-#%% FBMSNet_MixConv
-class FBMSNet(nn.Module):
+#%% sinc_conv
+def flip(x, dim):
+    xsize = x.size()
+    dim = x.dim() + dim if dim < 0 else dim
+    x = x.contiguous()
+    x = x.view(-1, *xsize[dim:])
+    x = x.view(x.size(0), x.size(1), -1)[:, getattr(torch.arange(x.size(1)-1,
+                      -1, -1), ('cpu','cuda')[x.is_cuda])().long(), :]
+    return x.view(xsize)
+
+def sinc(band,t_right):
+    y_right= torch.sin(2*math.pi*band*t_right)/(2*math.pi*band*t_right)
+    y_left= flip(y_right,0)
+
+    y=torch.cat([y_left,Variable(torch.ones(1)).cuda(),y_right])
+
+    return y
+
+class sinc_conv(nn.Module):
+    def __init__(self, N_filt, Filt_dim, fs):
+        super(sinc_conv, self).__init__()
+        low_freq_mel = 4
+        high_freq_mel = 38
+
+        np.random.seed(42)
+        b1 = np.random.uniform(low_freq_mel,high_freq_mel, N_filt)
+        b2 = np.zeros_like(b1) + 2
+
+        self.freq_scale = fs * 1.0
+        self.filt_b1 = nn.Parameter(torch.from_numpy(b1 / self.freq_scale))
+        self.filt_band = nn.Parameter(torch.from_numpy(b2 / self.freq_scale))
+
+        self.N_filt = N_filt
+        self.Filt_dim = Filt_dim
+        self.fs = fs
+
+    def forward(self, x):
+        filters = Variable(torch.zeros((self.N_filt, self.Filt_dim))).cuda()
+        N = self.Filt_dim
+        t_right = Variable(torch.linspace(1, (N - 1) / 2, steps=int((N - 1) / 2)) / self.fs).cuda()
+
+        min_freq = 1.0
+        min_band = 2.0
+
+        filt_beg_freq = torch.abs(self.filt_b1) +  min_freq / self.freq_scale
+        filt_end_freq = torch.clamp(filt_beg_freq + (torch.abs(self.filt_band) + min_band / self.freq_scale), 3/self.freq_scale, (self.fs/2)/self.freq_scale)
+        n = torch.linspace(0, N, steps=N)
+
+        window = 0.54 - 0.46 * torch.cos(2 * math.pi * n / N);
+        window = Variable(window.float().cuda())
+
+        for i in range(self.N_filt):
+            low_pass1 = 2 * filt_beg_freq[i].float() * sinc(filt_beg_freq[i].float() * self.freq_scale, t_right)
+            low_pass2 = 2 * filt_end_freq[i].float() * sinc(filt_end_freq[i].float() * self.freq_scale, t_right)
+            band_pass = (low_pass2 - low_pass1)
+
+            band_pass = band_pass / torch.max(band_pass)   #normalize to one
+
+            filters[i, :] = band_pass.cuda() * window
+        x = x.cuda()
+        out = F.conv2d(x, filters.view(self.N_filt, 1, 1,self.Filt_dim),padding=(0, int((self.Filt_dim - 1) / 2)))
+        return out
+
+#%% SincMSNet
+class SincMSNet(nn.Module):
     def SCB(self, in_chan, out_chan, nChan, doWeightNorm=True, *args, **kwargs):
         '''
         The spatial convolution block
@@ -251,11 +316,14 @@ class FBMSNet(nn.Module):
             nn.LogSoftmax(dim=1))
     def __init__(self, nChan, nTime, nClass=4, temporalLayer='LogVarLayer', num_Feat=36, dilatability=8, dropoutP=0.5, *args, **kwargs):
         # input_size: channel x datapoint
-        super(FBMSNet, self).__init__()
+        super(SincMSNet, self).__init__()
+        self.inception_window = [0.5, 0.25, 0.125, 0.0625]
         self.strideFactor = 4
 
+        self.sinc_conv = sinc_conv(N_filt=18, Filt_dim = 31, fs=250)
+
         self.mixConv2d = nn.Sequential(
-            MixedConv2d(in_channels=9, out_channels=num_Feat, kernel_size=[(1,15),(1,31),(1,63),(1,125)],
+            MixedConv2d(in_channels=18, out_channels=num_Feat, kernel_size=[(1,15),(1,31),(1,63),(1,125)],
                          stride=1, padding='', dilation=1, depthwise=False,),
             nn.BatchNorm2d(num_Feat),
         )
@@ -264,13 +332,13 @@ class FBMSNet(nn.Module):
         # Formulate the temporal agreegator
         self.temporalLayer = current_module.__dict__[temporalLayer](dim=3)
 
-        size = self.get_size(nChan, nTime)
-
-        self.fc = self.LastBlock(size[1],nClass)
+        self.fc = self.LastBlock(1152, nClass)
 
     def forward(self, x):
         if len(x.shape) == 5:
             x = torch.squeeze(x.permute((0, 4, 2, 3, 1)), dim=4)
+        x = x.float()
+        x = self.sinc_conv(x)
         y = self.mixConv2d(x)
         x = self.scb(y)
         x = x.reshape([*x.shape[0:2], self.strideFactor, int(x.shape[3] / self.strideFactor)])
@@ -279,105 +347,9 @@ class FBMSNet(nn.Module):
         c = self.fc(f)
         return c, f
 
-    def get_size(self, nChan, nTime):
-        data = torch.ones((1, 9, nChan, nTime))
-        x = self.mixConv2d(data)
-        x = self.scb(x)
-        x = x.reshape([*x.shape[0:2], self.strideFactor, int(x.shape[3] / self.strideFactor)])
-        x = self.temporalLayer(x)
-        x = torch.flatten(x, start_dim=1)
-        return x.size()
-
-#%% FBMSNet_Inception
-class FBMSNet_Inception(nn.Module):
-    def SCB(self, in_chan, out_chan, nChan, doWeightNorm=True, *args, **kwargs):
-        '''
-        The spatial convolution block
-        m : number of sptatial filters.
-        nBands: number of bands in the data
-        '''
-        return nn.Sequential(
-            Conv2dWithConstraint(in_chan, out_chan, (nChan, 1), groups=in_chan,
-                                 max_norm=2, doWeightNorm=doWeightNorm, padding=0),
-            nn.BatchNorm2d(out_chan),
-            swish()
-        )
-    def LastBlock(self, inF, outF, doWeightNorm=True, *args, **kwargs):
-        return nn.Sequential(
-            LinearWithConstraint(inF, outF, max_norm=0.5, doWeightNorm=doWeightNorm, *args, **kwargs),
-            nn.LogSoftmax(dim=1))
-    def __init__(self, nChan, nTime, nClass=4,sampling_rate=250, temporalLayer='LogVarLayer', num_Feat=36, dilatability=8, dropoutP=0.5, *args, **kwargs):
-        # input_size: channel x datapoint
-        super(FBMSNet_Inception, self).__init__()
-        self.inception_window = [0.5, 0.25, 0.125, 0.0625]
-        self.strideFactor = 4
-
-        # by setting the convolutional kernel being (1,lenght) and the strids being 1 we can use conv2d to
-        # achieve the 1d convolution operation
-        self.Tception1 = nn.Sequential(
-            nn.Conv2d(9, 9, kernel_size=(1, int(self.inception_window[0] * sampling_rate)), stride=1, padding=(0,int(self.inception_window[0] * sampling_rate/2))),
-            nn.BatchNorm2d(9),
-        )
-        self.Tception2 = nn.Sequential(
-            nn.Conv2d(9, 9, kernel_size=(1, int(self.inception_window[1] * sampling_rate-1)), stride=1, padding=(0,int(self.inception_window[1] * sampling_rate/2-1))),
-            nn.BatchNorm2d(9),
-        )
-        self.Tception3 = nn.Sequential(
-            nn.Conv2d(9, 9, kernel_size=(1, int(self.inception_window[2] * sampling_rate)), stride=1, padding=(0,int(self.inception_window[2] * sampling_rate/2))),
-            nn.BatchNorm2d(9),
-        )
-        self.Tception4 = nn.Sequential(
-            nn.Conv2d(9, 9, kernel_size=(1, int(self.inception_window[3] * sampling_rate)), stride=1, padding=(0,int(self.inception_window[3] * sampling_rate/2))),
-            nn.BatchNorm2d(9),
-        )
-        self.scb = self.SCB(in_chan=36, out_chan=288, nChan=int(nChan))
-
-        # Formulate the temporal agreegator
-        self.temporalLayer = current_module.__dict__[temporalLayer](dim=3)
-
-        size = self.get_size(nChan, nTime)
-
-        self.fc = self.LastBlock(size[1],nClass)
-
-    def forward(self, x):
-        if len(x.shape) == 5:
-            x = torch.squeeze(x.permute((0, 4, 2, 3, 1)), dim=4)
-        y = self.Tception1(x)
-        out = y
-        y = self.Tception2(x)
-        out = torch.cat((out, y), dim=1)
-        y = self.Tception3(x)
-        out = torch.cat((out, y), dim=1)
-        y = self.Tception4(x)
-        out = torch.cat((out, y), dim=1)
-        out = self.scb(out)
-        out = out.reshape([*out.shape[0:2], self.strideFactor, int(out.shape[3] / self.strideFactor)])
-        out = self.temporalLayer(out)
-        f = torch.flatten(out, start_dim=1)
-        c = self.fc(f)
-        return c,f
-
-    def get_size(self, nChan, nTime):
-        data = torch.ones((1, 9, nChan, nTime))
-        y = self.Tception1(data)
-        out = y
-        y = self.Tception2(data)
-        out = torch.cat((out, y), dim=1)
-        y = self.Tception3(data)
-        out = torch.cat((out, y), dim=1)
-        y = self.Tception4(data)
-        out = torch.cat((out, y), dim=1)
-        x = self.scb(out)
-        x = x.reshape([*x.shape[0:2], self.strideFactor, int(x.shape[3] / self.strideFactor)])
-        x = self.temporalLayer(x)
-        x = torch.flatten(x, start_dim=1)
-        return x.size()
 
 #%% __main__
 if __name__ == "__main__":
 
-    net = FBMSNet_Inception(nChan=22, nTime=512).cuda()
-    summary(net, (9, 22, 512))
-
-    net = FBMSNet(nChan=22, nTime=512).cuda()
-    summary(net, (9, 22, 512))
+    net = SincMSNet(nChan=22, nTime=512).cuda()
+    summary(net, (1, 22, 512))
